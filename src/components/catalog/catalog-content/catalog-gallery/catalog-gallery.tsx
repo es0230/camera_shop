fc1a0e51c526d39cff7d1389fe1ff68fa3760de5
Aleksandr Molodykh <caneyok@gmail.com>
2022-09-23 20:19:05 +0300

    Созданы компоненты для страницы каталога

---
+++ src/components/catalog/catalog-content/catalog-gallery/catalog-gallery.tsx
@@ -0,0 +1,11 @@
+import CatalogCard from '../catalog-card/catalog-card';
+
+function CatalogGallery(): JSX.Element {
+  return (
+    <div className="cards catalog__cards">
+      <CatalogCard />
+    </div>
+  );
+}
+
+export default CatalogGallery;
---
+++ src/const.ts
@@ -0,0 +1,8 @@
+enum AppRoute {
+  Basket = '/basket',
+  Catalog = '/catalog',
+  Product = '/product',
+  Unknown = '*',
+}
+
+export { AppRoute };
