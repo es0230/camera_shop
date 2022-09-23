bb02db1aab08147b43b5a6bfba447aa89fb036d4
Aleksandr Molodykh <caneyok@gmail.com>
2022-09-23 20:16:53 +0300

    Настроен router

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import Basket from '../../pages/basket/basket';
+import Catalog from '../../pages/catalog/catalog';
+import Product from '../../pages/product/product';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Basket} element={<Basket />} />
+        <Route path={AppRoute.Catalog} element={<Catalog />} />
+        <Route path={AppRoute.Product} element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+
+  );
 }
 
 export default App;
