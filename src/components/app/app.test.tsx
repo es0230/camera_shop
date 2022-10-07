ebe1e80eb36017971bf1c969865be33864ef39cf
Aleksandr Molodykh <caneyok@gmail.com>
2022-10-07 16:41:05 +0300

    Протестирована часть маршрутизации

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,8 +1,41 @@
-import {render, screen} from '@testing-library/react';
+/**
+ * @jest-environment jsdom
+ */
+
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Provider } from 'react-redux';
+import { AppRoute } from '../../const';
+import { makeFakeCamera, makeFakePromo } from '../../mocks/mocks';
+import HistoryRouter from '../history-router/history-router';
+import '@testing-library/jest-dom';
 import App from './app';
 
-test('Renders app-component', () => {
-  render(<App />);
-  const textElement = screen.getByText(/Hello, world!/i);
-  expect(textElement).toBeInTheDocument();
+const mockStore = configureMockStore();
+
+const store = mockStore({
+  DATA: { cameras: [makeFakeCamera()], promo: makeFakePromo(), isDataLoaded: false },
+});
+
+const history = createMemoryHistory();
+
+const fakeApp = (
+  <Provider store={store}>
+    <HistoryRouter history={history}>
+      <App />
+    </HistoryRouter>
+  </Provider>
+);
+
+describe('Testing App component', () => {
+  it('should render Catalog when user navigate to "/catalog/1"', async () => {
+    history.push(AppRoute.Catalog(1));
+
+    render(fakeApp);
+
+    expect(history.location.pathname).toBe(AppRoute.Catalog(1));
+
+    expect(screen.getByText(/Каталог фото- и видеотехники/i)).toBeInTheDocument();
+  });
 });
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import Catalog from '../../pages/catalog/catalog';
 import PageNotFound from '../../pages/page-not-found/page-not-found';
@@ -6,14 +6,11 @@ import Product from '../../pages/product/product';
 
 function App(): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={AppRoute.Catalog()} element={<Catalog />} />
-        <Route path={AppRoute.Product()} element={<Product />} />
-        <Route path={AppRoute.Unknown()} element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-
+    <Routes>
+      <Route path={AppRoute.Catalog()} element={<Catalog />} />
+      <Route path={AppRoute.Product()} element={<Product />} />
+      <Route path={AppRoute.Unknown()} element={<PageNotFound />} />
+    </Routes>
   );
 }
 
