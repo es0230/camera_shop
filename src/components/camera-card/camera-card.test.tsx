9d9e448dd9bfc0ffa0405e714ad2dbcd3e6df3dd
Aleksandr Molodykh <caneyok@gmail.com>
2022-10-09 14:52:08 +0300

    Добавлен тест для карточки товара

--- src/components/camera-card/camera-card.test.tsx
+++ src/components/camera-card/camera-card.test.tsx
@@ -2,12 +2,19 @@
  * @jest-environment jsdom
  */
 
+import { configureMockStore } from '@jedmao/redux-mock-store';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { AppRoute, TabType } from '../../const';
 import { makeFakeCamera } from '../../mocks/mocks';
+import HistoryRouter from '../history-router/history-router';
 import CameraCard from './camera-card';
 
+const mockStore = configureMockStore();
 describe('Testing CameraCard component', () => {
   const mockCamera = makeFakeCamera();
 
@@ -24,5 +31,22 @@ describe('Testing CameraCard component', () => {
     expect(screen.getByText(mockCamera.price)).toBeInTheDocument();
     expect(screen.getByText('Купить')).toBeInTheDocument();
     expect(screen.getByText('Подробнее')).toBeInTheDocument();
+  });
+
+  it('should navigate to Product on button click', async () => {
+    const history = createMemoryHistory();
+
+    render(
+      <Provider store={mockStore({})}>
+        <HistoryRouter history={history}>
+          <CameraCard camera={mockCamera} />
+        </HistoryRouter>
+      </Provider>
+    );
+
+    const button = screen.getByText(/Подробнее/i);
+
+    await userEvent.click(button);
+    expect(history.location.pathname).toBe(AppRoute.Product(mockCamera.id, TabType.Perks));
   });
 });
