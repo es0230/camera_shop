c0af3a7945945b2a0fd42cdde18214e68b9abfaa
Aleksandr Molodykh <caneyok@gmail.com>
2022-10-17 13:35:03 +0300

    Исправлены баги в компоненте пагинации

--- src/components/catalog/catalog-content/catalog-pagination/catalog-pagination.tsx
+++ src/components/catalog/catalog-content/catalog-pagination/catalog-pagination.tsx
@@ -6,68 +6,27 @@ type CatalogPaginationProps = {
   totalPageAmount: number,
 }
 function CatalogPagination({ currentPage, totalPageAmount }: CatalogPaginationProps): JSX.Element {
-  switch (currentPage) {
-    case 1:
-      return (
-        <div data-testid="catalog-pagination-component" className="pagination">
-          <ul className="pagination__list">
-            <li className="pagination__item">
-              <Link className="pagination__link pagination__link--active" to={AppRoute.Catalog(currentPage)}>1</Link>
-            </li>
-            <li className="pagination__item">
-              <Link className="pagination__link" to={AppRoute.Catalog(currentPage + 1)}>2</Link>
-            </li>
-            <li className="pagination__item">
-              <Link className="pagination__link" to={AppRoute.Catalog(currentPage + 2)}>3</Link>
-            </li>
-            <li className="pagination__item">
-              <Link className="pagination__link pagination__link--text" to={AppRoute.Catalog(currentPage + 1)}>Далее</Link>
-            </li>
-          </ul>
-        </div>
-      );
-    case totalPageAmount:
-      return (
-        <div data-testid="catalog-pagination-component" className="pagination">
-          <ul className="pagination__list">
-            <li className="pagination__item">
-              <Link className="pagination__link pagination__link--text" to={AppRoute.Catalog(currentPage - 1)}>Назад</Link>
-            </li>
-            <li className="pagination__item">
-              <Link className="pagination__link" to={AppRoute.Catalog(currentPage - 2)}>{currentPage - 2}</Link>
-            </li>
-            <li className="pagination__item">
-              <Link className="pagination__link" to={AppRoute.Catalog(currentPage - 1)}>{currentPage - 1}</Link>
-            </li>
-            <li className="pagination__item">
-              <Link className="pagination__link pagination__link--active" to={AppRoute.Catalog(currentPage)}>{totalPageAmount}</Link>
-            </li>
-          </ul>
-        </div>
-      );
-    default:
-      return (
-        <div data-testid="catalog-pagination-component" className="pagination">
-          <ul className="pagination__list">
-            <li className="pagination__item">
-              <Link className="pagination__link pagination__link--text" to={AppRoute.Catalog(currentPage - 1)}>Назад</Link>
-            </li>
-            <li className="pagination__item">
-              <Link className="pagination__link" to={AppRoute.Catalog(currentPage - 1)}>{currentPage - 1}</Link>
-            </li>
-            <li className="pagination__item">
-              <Link className="pagination__link pagination__link--active" to={AppRoute.Catalog(currentPage)}>{currentPage}</Link>
-            </li>
-            <li className="pagination__item">
-              <Link className="pagination__link" to={AppRoute.Catalog(currentPage + 1)}>{currentPage + 1}</Link>
-            </li>
-            <li className="pagination__item">
-              <Link className="pagination__link pagination__link--text" to={AppRoute.Catalog(currentPage + 1)}>Далее</Link>
-            </li>
-          </ul>
-        </div>
-      );
-  }
+  return (
+    <div data-testid="catalog-pagination-component" className="pagination">
+      <ul className="pagination__list">
+        {currentPage !== 1 ?
+          <li className="pagination__item">
+            <Link className="pagination__link pagination__link--text" to={AppRoute.Catalog(currentPage - 1)}>Назад</Link>
+          </li> :
+          <> </>}
+        {Array.from({ length: totalPageAmount }, (el, i) => (
+          <li className="pagination__item" key={i}>
+            <Link className={`pagination__link ${currentPage === i + 1 ? 'pagination__link--active' : ''}`} to={AppRoute.Catalog(i + 1)}>{i + 1}</Link>
+          </li>
+        ))}
+        {currentPage !== totalPageAmount ?
+          <li className="pagination__item">
+            <Link className="pagination__link pagination__link--text" to={AppRoute.Catalog(currentPage + 1)}>Далее</Link>
+          </li> :
+          <> </>}
+      </ul>
+    </div>
+  );
 }
 
 export default CatalogPagination;
